a8528d8dc59d24adc679d4e626ad03e57c5e1315
Michael Orr <michael@cloudspace.com>
2016-02-22 15:26:14 -0500

    interpret twist message and determine if we are moving forward, left, or right

--- arduino/mobile_base/mobile_base.ino
+++ arduino/mobile_base/mobile_base.ino
@@ -6,8 +6,22 @@ ros::NodeHandle  nh;
 
 void messageCb(const geometry_msgs::Twist& msg)
 {
+  int forward;
+  int turn;
   digitalWrite(13, HIGH-digitalRead(13));
-  Serial.println("hi!----------------------");
+  
+  forward = msg.linear.x;
+  turn = msg.angular.z;
+  if(forward == 1){
+    Serial.println("MOVING FORWARD");    
+  } else {
+    if(turn == 1){
+      Serial.println("TURN LEFT");
+    }
+    if(turn == -1){
+      Serial.println("TURN RIGHT");
+    }
+  }
 }
 
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", messageCb);
@@ -19,7 +33,6 @@ void setup(){
 }
 
 void loop(){
-  Serial.println("loop");
   nh.spinOnce();
-  delay(200);
+  delay(1);
 }
