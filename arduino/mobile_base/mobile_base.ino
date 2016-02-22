5b48d9ee4292b064c1e1313ed22f1e71d6f58d1a
Michael Orr <michael@cloudspace.com>
2016-02-22 17:18:39 -0500

    first pass at code for driving motors with no actual running of the code - concerned that there should be a second parameter in the motor 1, 2 declarations after the pin 3, 4

--- arduino/mobile_base/mobile_base.ino
+++ arduino/mobile_base/mobile_base.ino
@@ -1,8 +1,11 @@
 #include <Arduino.h>
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
+#include <AFMotor.h>
 
 ros::NodeHandle  nh;
+AF_DCMotor motor1(3);
+AF_DCMotor motor2(4);
 
 void messageCb(const geometry_msgs::Twist& msg)
 {
@@ -13,13 +16,22 @@ void messageCb(const geometry_msgs::Twist& msg)
   forward = msg.linear.x;
   turn = msg.angular.z;
   if(forward == 1){
-    Serial.println("MOVING FORWARD");    
+    Serial.println("MOVING FORWARD"); 
+    motor1.run(FORWARD);
+    motor2.run(FORWARD);
+    delay(10);
   } else {
     if(turn == 1){
       Serial.println("TURN LEFT");
+      motor1.run(FORWARD);
+      motor2.run(BACKWARD);
+      delay(10);
     }
     if(turn == -1){
       Serial.println("TURN RIGHT");
+      motor1.run(BACKWARD);
+      motor2.run(FORWARD);
+      delay(10);
     }
   }
 }
@@ -27,9 +39,14 @@ void messageCb(const geometry_msgs::Twist& msg)
 ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", messageCb);
 
 void setup(){
+  Serial.begin(9600);
   pinMode(13, OUTPUT);
   nh.initNode();
   nh.subscribe(sub);
+  motor1.setSpeed(200);
+  motor1.run(RELEASE);
+  motor2.setSpeed(200);
+  motor2.run(RELEASE);
 }
 
 void loop(){
