4758265c948764f74150b400d3e19744d9576e56
Michael Orr <michael@cloudspace.com>
2016-02-23 11:14:37 -0500

    backwards action

--- arduino/mobile_base/mobile_base.ino
+++ arduino/mobile_base/mobile_base.ino
@@ -6,46 +6,59 @@
 ros::NodeHandle  nh;
 AF_DCMotor motor1(3);
 AF_DCMotor motor2(4);
-int baseSpeed = 125;
+int turnSpeed = 125;
+int moveSpeed = 175;
 int running = 0;
 
 void messageCb(const geometry_msgs::Twist& msg)
 {
-  int forward;
+  int movement;
   int turn;
   if(running == 0){
-    forward = msg.linear.x;
+    movement = msg.linear.x;
     turn = msg.angular.z;
-    if(forward == 1 || turn == 1 || turn == -1){
+    if(movement == 1 || movement == -1 || turn == 1 || turn == -1){
       digitalWrite(13, HIGH);
       running = 1;
-      if(forward == 1){
-        Serial.println("MOVING FORWARD"); 
-        motor1.run(FORWARD);
-        motor1.setSpeed(baseSpeed);
-        motor2.run(FORWARD);
-        motor2.setSpeed(baseSpeed);
+      if(movement == 1 || movement == -1){
+        if(movement == 1){
+          Serial.println("MOVING FORWARD"); 
+          motor1.run(FORWARD);
+          motor1.setSpeed(moveSpeed);
+          motor2.run(FORWARD);
+          motor2.setSpeed(moveSpeed);
+        }
+        if(movement == -1){
+          Serial.println("MOVING BACKWARD"); 
+          motor1.run(BACKWARD);
+          motor1.setSpeed(moveSpeed);
+          motor2.run(BACKWARD);
+          motor2.setSpeed(moveSpeed);          
+        }
       } else {
         if(turn == 1){
           Serial.println("TURN LEFT");
           motor1.run(BACKWARD);
-          motor1.setSpeed(baseSpeed);
+          motor1.setSpeed(turnSpeed);
           motor2.run(FORWARD);
-          motor2.setSpeed(baseSpeed);
+          motor2.setSpeed(turnSpeed);
         }
         if(turn == -1){
           Serial.println("TURN RIGHT");
           motor1.run(FORWARD);
-          motor1.setSpeed(baseSpeed);
+          motor1.setSpeed(turnSpeed);
           motor2.run(BACKWARD);
-          motor2.setSpeed(baseSpeed);
+          motor2.setSpeed(turnSpeed);
         }
       }
-      delay(199);
-      motor1.run(RELEASE);
-      motor2.run(RELEASE);
-      digitalWrite(13, LOW);
+      delay(300);
       running = 0;
+      delay(50);
+      if(running == 0){
+        motor1.run(RELEASE);
+        motor2.run(RELEASE);
+        digitalWrite(13, LOW);
+      }
     }
   }
 }
@@ -57,9 +70,9 @@ void setup(){
   pinMode(13, OUTPUT);
   nh.initNode();
   nh.subscribe(sub);
-  motor1.setSpeed(baseSpeed);
+  motor1.setSpeed(turnSpeed);
   motor1.run(RELEASE);
-  motor2.setSpeed(baseSpeed);
+  motor2.setSpeed(turnSpeed);
   motor2.run(RELEASE);
 }
 
